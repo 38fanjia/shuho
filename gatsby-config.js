bfac6c770cc81a65a16daa4d8606821cf87a27e0
fanjia38 <38fanjia@gmail.com>
2019-08-19 23:40:31 +0900

    :wrench: fix lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: "/shuho",
+  pathPrefix: '/shuho',
   siteMetadata: {
     siteTitle: `shuho`,
     siteDescription: `fanjia38 中文的周记`,
